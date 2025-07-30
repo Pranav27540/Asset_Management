85c514514f48d195aae7e00d2e7b6f5be332d7af
Pranav Amale <154261196+Pranav27540@users.noreply.github.com>
2025-07-30 23:34:34 +0530

    Create App.vue

---
+++ frontend/src/frontend/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Asset Management System</h1>
+  <AddAsset />
+  <AssetList />
+</template>
+
+<script setup>
+import AddAsset from './components/AddAsset.vue'
+import AssetList from './components/AssetList.vue'
+</script>
