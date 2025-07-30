1aee2bb22dc1f69efcd2cfd5053f90f0d404f23b
Pranav Amale <154261196+Pranav27540@users.noreply.github.com>
2025-07-30 23:36:12 +0530

    Create AssetList.vue

---
+++ frontend/src/components/AssetList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>Asset List</h2>
+    <ul>
+      <li v-for="asset in assets" :key="asset.id">
+        {{ asset.name }} ({{ asset.category }}) - {{ asset.status }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import axios from 'axios'
+import { ref, onMounted } from 'vue'
+
+const assets = ref([])
+
+const loadAssets = async () => {
+  const res = await axios.get('http://localhost:8080/api/assets')
+  assets.value = res.data
+}
+
+onMounted(() => loadAssets())
+</script>
